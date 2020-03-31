10ea9cd1ca0cca18522b7c488e39937baf99476d
zbs <250468591@qq.com>
2020-03-31 16:19:14 +0800

    如果没有匿名内部类，就需要创建子类，但是，如果此处的Son类只使用一次， 那么将其编写为独立的一个类岂不是很麻烦？这个时候就引入了匿名内部类。

--- src/main/java/com/zbs/anonymityClass/nonAnonymityDemo2.java
+++ src/main/java/com/zbs/anonymityClass/nonAnonymityDemo2.java
@@ -7,4 +7,13 @@ package com.zbs.anonymityClass;
  * version: 1.0
  */
 public class nonAnonymityDemo2 {
+    public static void main(String[] args) {
+        Father father=new Son();
+        /**
+         * 子类实例向上转型为父类的引用，
+         * 父类引用具有子类的特点。
+         */
+        //打印：我是子类
+        father.simplePrint();
+    }
 }
