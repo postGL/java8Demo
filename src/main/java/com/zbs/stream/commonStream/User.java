a3f3235918a4da173b522dbbe65446c9d9eee57f
zbs <zhangbaoshuai@cph.citic>
2020-11-11 20:27:12 +0800

    fix(java8): 简化代码

--- src/main/java/com/zbs/stream/commonStream/User.java
+++ src/main/java/com/zbs/stream/commonStream/User.java
@@ -1,46 +1,21 @@
 package com.zbs.stream.commonStream;
 
+import lombok.Data;
+
 /**
  * description: User
  * date: 2020/7/31 16:46
  * author: zhangbs
  * version: 1.0
  */
+@Data
 public class User {
     private Long id;
-    private String username;
+    private String userName;
+    private Integer userId;
     private Integer age;
+    private Integer gender;
+    private String phone;
+    private String address;
 
-    public User() {
-    }
-
-    public User(Long id, String username, Integer age) {
-        this.id = id;
-        this.username = username;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
