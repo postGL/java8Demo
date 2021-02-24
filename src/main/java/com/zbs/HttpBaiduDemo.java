62231e9edeb5ba8d3e820de57024450574d64b7d
zbs <zhangbaoshuai@cph.citic>
2021-02-24 14:56:20 +0800

    feat: java 执行打开页面

---
+++ src/main/java/com/zbs/HttpBaiduDemo.java
@@ -0,0 +1,16 @@
+package com.zbs;
+
+import java.io.IOException;
+
+/**
+ * description: HttpBaiduDemo
+ * date: 2020/11/17 11:27
+ * author: zhangbs
+ * version: 1.0
+ */
+public class HttpBaiduDemo {
+
+    public static void main(String[] args) throws IOException {
+        Runtime.getRuntime().exec("cmd /c start http://www.baidu.com");
+    }
+}
